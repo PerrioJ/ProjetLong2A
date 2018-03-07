0056240812082ce16c1b039c685a059e3ee130ce
Jules Perrio <PerrioJ@gmail.com>
2018-03-07 15:54:51 +0100

    Dictée de note : on a ajouté les #

--- app/src/main/java/perriobarreteau/apprentissagemusique/DicteeDeNote.java
+++ app/src/main/java/perriobarreteau/apprentissagemusique/DicteeDeNote.java
@@ -13,14 +13,19 @@ import java.util.Random;
 
 public class DicteeDeNote extends AppCompatActivity implements View.OnClickListener {
 
-    Note Do = new Note(1, "Do", R.drawable.ic_note_do, R.raw.aaa);
+    Note Do = new Note(0, "Do", R.drawable.ic_note_do, R.raw.aaa);
+    Note Dod = new Note(1, "Do#", R.drawable.ic_note_do, R.raw.aaa);
     Note Re = new Note(2, "Ré", R.drawable.ic_note_re, R.raw.re);
-    Note Mi = new Note(3, "Mi", R.drawable.ic_note_mi, R.raw.mi);
-    Note Fa = new Note(4, "Fa", R.drawable.ic_note_fa, R.raw.fa);
-    Note Sol = new Note(5, "Sol", R.drawable.ic_note_sol, R.raw.sol);
-    Note La = new Note(6, "La", R.drawable.ic_note_la, R.raw.la);
-    Note Si = new Note(7, "Si", R.drawable.ic_note_si, R.raw.si);
-    Note[] notes = {Do, Re, Mi, Fa, Sol, La, Si};
+    Note Red = new Note(3, "Ré#", R.drawable.ic_note_re, R.raw.re);
+    Note Mi = new Note(4, "Mi", R.drawable.ic_note_mi, R.raw.mi);
+    Note Fa = new Note(5, "Fa", R.drawable.ic_note_fa, R.raw.fa);
+    Note Fad = new Note(6, "Fa#", R.drawable.ic_note_fa, R.raw.fa);
+    Note Sol = new Note(7, "Sol", R.drawable.ic_note_sol, R.raw.sol);
+    Note Sold = new Note(8, "Sol#", R.drawable.ic_note_sol, R.raw.sol);
+    Note La = new Note(9, "La", R.drawable.ic_note_la, R.raw.la);
+    Note Lad = new Note(10, "La#", R.drawable.ic_note_la, R.raw.la);
+    Note Si = new Note(11, "Si", R.drawable.ic_note_si, R.raw.si);
+    Note[] notes = {Do, Dod, Re, Red, Mi, Fa, Fad, Sol, Sold, La, Lad, Si};
     MediaPlayer mediaplayer;
     int idRandom = 0; // Choix aléatoire de la note à deviner
     int nbEcoutes = 0;
@@ -33,14 +38,14 @@ public class DicteeDeNote extends AppCompatActivity implements View.OnClickListe
 
         // Sélection de la note à deviner
         Random rand = new Random();
-        this.idRandom = rand.nextInt(7) + 1;
+        this.idRandom = rand.nextInt(12);
 
         ImageButton imageButtonNote = (ImageButton) findViewById(R.id.imageButtonNote);
         imageButtonNote.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 nbEcoutes ++;
                 if (nbEcoutes <= 5) {
-                    mediaplayer = MediaPlayer.create(getApplicationContext(), notes[idRandom-1].son);
+                    mediaplayer = MediaPlayer.create(getApplicationContext(), notes[idRandom].son);
                     mediaplayer.start();
                 }
                 else {
@@ -50,25 +55,51 @@ public class DicteeDeNote extends AppCompatActivity implements View.OnClickListe
         });
 
         Button buttonDo = (Button) findViewById(R.id.buttonDo);
-        buttonDo.setTag(Integer.valueOf(1));
+        buttonDo.setTag(Integer.valueOf(0));
         buttonDo.setOnClickListener(this);
+
+        Button buttonDod = (Button) findViewById(R.id.buttonDod);
+        buttonDod.setTag(Integer.valueOf(1));
+        buttonDod.setOnClickListener(this);
+
         Button buttonRe = (Button) findViewById(R.id.buttonRe);
         buttonRe.setTag(Integer.valueOf(2));
         buttonRe.setOnClickListener(this);
+
+        Button buttonRed = (Button) findViewById(R.id.buttonRed);
+        buttonRed.setTag(Integer.valueOf(3));
+        buttonRed.setOnClickListener(this);
+
         Button buttonMi = (Button) findViewById(R.id.buttonMi);
-        buttonMi.setTag(Integer.valueOf(3));
+        buttonMi.setTag(Integer.valueOf(4));
         buttonMi.setOnClickListener(this);
+
         Button buttonFa = (Button) findViewById(R.id.buttonFa);
-        buttonFa.setTag(Integer.valueOf(4));
+        buttonFa.setTag(Integer.valueOf(5));
         buttonFa.setOnClickListener(this);
+
+        Button buttonFad = (Button) findViewById(R.id.buttonFad);
+        buttonFad.setTag(Integer.valueOf(6));
+        buttonFad.setOnClickListener(this);
+
         Button buttonSol = (Button) findViewById(R.id.buttonSol);
-        buttonSol.setTag(Integer.valueOf(5));
+        buttonSol.setTag(Integer.valueOf(7));
         buttonSol.setOnClickListener(this);
+
+        Button buttonSold = (Button) findViewById(R.id.buttonSold);
+        buttonSold.setTag(Integer.valueOf(8));
+        buttonSold.setOnClickListener(this);
+
         Button buttonLa = (Button) findViewById(R.id.buttonLa);
-        buttonLa.setTag(Integer.valueOf(6));
+        buttonLa.setTag(Integer.valueOf(9));
         buttonLa.setOnClickListener(this);
+
+        Button buttonLad = (Button) findViewById(R.id.buttonLad);
+        buttonLad.setTag(Integer.valueOf(10));
+        buttonLad.setOnClickListener(this);
+
         Button buttonSi = (Button) findViewById(R.id.buttonSi);
-        buttonSi.setTag(Integer.valueOf(7));
+        buttonSi.setTag(Integer.valueOf(11));
         buttonSi.setOnClickListener(this);
 
     }
@@ -85,9 +116,9 @@ public class DicteeDeNote extends AppCompatActivity implements View.OnClickListe
             Toast.makeText(getApplicationContext(), "Bravo !", Toast.LENGTH_SHORT).show();
             // On choisi aléatoirement une nouvelle note à deviner et met à jour l'affichage
             Random rand = new Random();
-            int idRandomNew = rand.nextInt(7) + 1;
+            int idRandomNew = rand.nextInt(12);
             while (idRandomNew ==this.idRandom) {
-                idRandomNew = rand.nextInt(7) + 1;
+                idRandomNew = rand.nextInt(12);
             }
             this.idRandom = idRandomNew;
             this.nbFautes = 0;
@@ -102,9 +133,9 @@ public class DicteeDeNote extends AppCompatActivity implements View.OnClickListe
             else {
                 Toast.makeText(getApplicationContext(), "C'est faux... la bonne réponse était : " + notes[this.idRandom-1].nom, Toast.LENGTH_SHORT).show();
                 Random rand = new Random();
-                int idRandomNew = rand.nextInt(7) + 1;
+                int idRandomNew = rand.nextInt(12);
                 while (idRandomNew ==this.idRandom) {
-                    idRandomNew = rand.nextInt(7) + 1;
+                    idRandomNew = rand.nextInt(12);
                 }
                 this.idRandom = idRandomNew;
                 this.nbFautes = 0;
